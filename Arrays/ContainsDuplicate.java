ca9940d87717bfd56cc8da9391bd654c385990fd
Vibhanshu Jain <105918194+curious-companion@users.noreply.github.com>
2025-11-26 16:29:11 +0530

    Implement containsDuplicate method using HashSet

--- Arrays/ContainsDuplicate.java
+++ Arrays/ContainsDuplicate.java
@@ -1,1 +1,13 @@
-
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num: nums) {
+            if (set.contains(num)){
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
