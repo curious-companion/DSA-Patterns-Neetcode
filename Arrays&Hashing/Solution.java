75377a61e34d0cece3a649a3545374fd14b68108
= <vibhanshujain2003@gmail.com>
2025-11-28 16:58:05 +0530

    Add TopKFrequentElements file

---
+++ Arrays&Hashing/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Solution {
+    public int[] topKFrequent(int[] nums, int k){
+        int len = nums.length;
+        if (len == k) {
+            return nums;
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        Queue<Integer> heap = new PriorityQueue<>(
+            (n1,n2)->map.get(n1)-map.get(n2)
+        );
+
+        for(int n: map.keySet()) {
+            heap.add(n);
+            if( heap.size() > k) heap.poll();
+        }
+
+        int top[] = new int[k];
+        for(int i = k-1; i >= 0; i--) {
+            top[i] = heap.poll();
+        }
+        return top;
+    }
+}
