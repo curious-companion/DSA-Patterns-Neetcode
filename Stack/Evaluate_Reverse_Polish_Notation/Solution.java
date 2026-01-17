2cbb8c70e561015a3d70dbae3aaabac93e2c5bd5
Vibhanshu Jain <105918194+curious-companion@users.noreply.github.com>
2026-01-17 18:10:35 +0530

    Implement Reverse Polish Notation evaluation

---
+++ Stack/Evaluate_Reverse_Polish_Notation/Solution.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Solution {
+  public static int evalRPN(String[] tokens){
+    Deque<Integer> stack = new ArrayDeque<>();
+    for(Strinbg token: tokens) {
+      if (token.equals("+")) {
+        stack.push(stack.pop()+stack.pop());
+      } else if (token.equals("-")) {
+        int second = stack.pop();
+        int first = stack.pop();
+        stack.push(first-second);
+      } else if (token.equals("*") {
+        stack.push(stack.pop()*stack.pop());
+      } else if (token.equals("/") {
+        int second = stack.pop();
+        int first = stack.pop();
+        stack.push(first/second);
+      } else {
+        stack.push(Integer.parseInt(token));
+      }
+    }
+    return stack.pop();
+  }
+}
